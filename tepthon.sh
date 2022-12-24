3ed377f6abc513651b97dbe29d7a56e7bc99690f
Source Tepthon <121296616+TheTepthon@users.noreply.github.com>
2022-12-24 07:51:35 +0300

    Update tepthon.sh

--- tepthon.sh
+++ tepthon.sh
@@ -1,6 +1,6 @@
-branch=main
-git clone -b $branch https://github.com/anes10a/quran /root/anes10a
-cp quran/.env /root/anes10a/.env
-cd /root/anes10a
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t quran
-docker run --privileged --env-file .env --rm -i quran
+branch=master
+git clone -b $branch https://github.com/TheTepthon/music /root/music
+cp music/.env /root/TheTepthon/.env
+cd /root/TheTepthon
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t music
+docker run --privileged --env-file .env --rm -i music
